495eb1fa14a4b3cbfbb2647029d04aeb183606f9
ycazeau <ycazeau23@gmail.com>
2020-04-28 23:19:00 -0400

    Set Splash Activity to start before Main Activity

--- app/src/main/java/com/example/flyingfishgame/SplashActivity.java
+++ app/src/main/java/com/example/flyingfishgame/SplashActivity.java
@@ -2,7 +2,10 @@ package com.example.flyingfishgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.jar.JarEntry;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread()
+        {
+            @Override
+            public void run() {
+                try
+                {
+                    sleep(5000);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+                finally {
+                    Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
