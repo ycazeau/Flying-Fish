113451bccd47c72f8415af2e3ec68d7e6d60df78
ycazeau <ycazeau23@gmail.com>
2020-04-30 02:13:20 -0400

    Designing the Game Over Activity

--- app/src/main/java/com/example/flyingfishgame/GameOverActivity.java
+++ app/src/main/java/com/example/flyingfishgame/GameOverActivity.java
@@ -2,13 +2,28 @@ package com.example.flyingfishgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameOverActivity extends AppCompatActivity {
+
+    private Button StartGameAgain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        StartGameAgain = findViewById(R.id.btnPlayAgain);
+
+        StartGameAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(GameOverActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
     }
 }
