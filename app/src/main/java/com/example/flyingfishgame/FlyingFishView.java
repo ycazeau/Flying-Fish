306fbaad77a8c42c9486979b593e93e15d898791
ycazeau <ycazeau23@gmail.com>
2020-04-30 23:19:41 -0400

    Pass the score from Main Activity to GameOver activity

--- app/src/main/java/com/example/flyingfishgame/FlyingFishView.java
+++ app/src/main/java/com/example/flyingfishgame/FlyingFishView.java
@@ -157,6 +157,7 @@ public class FlyingFishView  extends View
 
                Intent gameOverIntent = new Intent(getContext(), GameOverActivity.class);
                gameOverIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+               gameOverIntent.putExtra("score", score);
                getContext().startActivity(gameOverIntent);
            }
 
--- app/src/main/java/com/example/flyingfishgame/GameOverActivity.java
+++ app/src/main/java/com/example/flyingfishgame/GameOverActivity.java
@@ -6,17 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class GameOverActivity extends AppCompatActivity {
 
     private Button StartGameAgain;
+    private TextView DisplayScore;
+    private String score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
 
+        score = getIntent().getExtras().get("score").toString();
+
         StartGameAgain = findViewById(R.id.btnPlayAgain);
+
+        DisplayScore = findViewById(R.id.displayScore);
 
         StartGameAgain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +32,7 @@ public class GameOverActivity extends AppCompatActivity {
                 startActivity(mainIntent);
             }
         });
+
+        DisplayScore.setText("Your Score is : " + score);
     }
 }
